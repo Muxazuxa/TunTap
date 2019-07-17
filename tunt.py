77424ce3d3eedd188d569a1d9ee94e2293edcf42
jenish <muxazuxa@mail.ru>
2019-07-17 11:52:49 +0600

    change mask

--- tunt.py
+++ tunt.py
@@ -3,7 +3,7 @@ from pytun import TunTapDevice, IFF_NO_PI, IFF_TUN
 tun = TunTapDevice(name="tun", flags=(IFF_TUN | IFF_NO_PI))
 tun.addr = '192.168.13.10'
 tun.dstaddr = '192.168.13.1'
-tun.netmask = '255.255.0.0'
+tun.netmask = '255.255.255.0'
 tun.persist(True)
 tun.mtu = 1500
 buf = tun.read(tun.mtu)
